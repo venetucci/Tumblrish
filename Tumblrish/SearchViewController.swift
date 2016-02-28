e62186f7a66d5e7ee1a506dcb024e0f3a9848d33
venetucci <michelle.ann.harvey@gmail.com>
2016-02-27 22:10:05 -0800

    Add loading indicator to search tab

--- Tumblrish/SearchViewController.swift
+++ Tumblrish/SearchViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var loadingView: UIImageView!
+    @IBOutlet weak var searchView: UIImageView!
+    
+    var loading_1: UIImage!
+    var loading_2: UIImage!
+    var loading_3: UIImage!
+    
+    var images: [UIImage]!
+    var animatedImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loading_1 = UIImage(named: "loading-1")
+        loading_2 = UIImage(named: "loading-2")
+        loading_3 = UIImage(named: "loading-3")
+        images = [loading_1, loading_2, loading_3]
+        animatedImage = UIImage.animatedImageWithImages(images, duration: 1.0)
+        loadingView.image = animatedImage
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        loadingView.alpha = 1
+        searchView.alpha = 0
+        
+        UIView.animateWithDuration(0, delay: 2, options: [ ], animations: { () -> Void in
+            self.loadingView.alpha = 0
+            self.searchView.alpha = 1
+            }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
